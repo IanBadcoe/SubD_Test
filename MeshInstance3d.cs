7a7c442b463821e5f6c2c64fc345197d1db85ccb
Ian Badcoe <igbadcoe@gmail.com>
2025-04-04 14:20:25 +0100

    Tweak test case to exercise face/edge/vert removal in BuildFromCubes.ToSurface

--- MeshInstance3d.cs
+++ MeshInstance3d.cs
@@ -23,12 +23,19 @@ public partial class MeshInstance3d : MeshInstance3D
     {
         BuildFromCubes bfc = new BuildFromCubes();
         bfc.SetCube(new Vector3I(0, 0, 0), 1);
-        bfc.SetCube(new Vector3I(2, 0, 0), 1);
-        bfc.SetCube(new Vector3I(2, 2, 0), 1);
-        bfc.SetCube(new Vector3I(0, 0, 2), 1);
-        bfc.SetCube(new Vector3I(0, -2, 0), 1);
-        bfc.SetCube(new Vector3I(0, 0, -2), 1);
+        bfc.SetCube(new Vector3I(1, 0, 0), 1);
+        bfc.SetCube(new Vector3I(0, 1, 0), 1);
+        bfc.SetCube(new Vector3I(1, 1, 0), 1);
+        bfc.SetCube(new Vector3I(0, 0, 1), 1);
+        bfc.SetCube(new Vector3I(1, 0, 1), 1);
+        bfc.SetCube(new Vector3I(0, 1, 1), 1);
         bfc.SetCube(new Vector3I(1, 1, 1), 1);
+        // bfc.SetCube(new Vector3I(2, 0, 0), 1);
+        // bfc.SetCube(new Vector3I(2, 2, 0), 1);
+        // bfc.SetCube(new Vector3I(0, 0, 2), 1);
+        // bfc.SetCube(new Vector3I(0, -2, 0), 1);
+        // bfc.SetCube(new Vector3I(0, 0, -2), 1);
+        // bfc.SetCube(new Vector3I(1, 1, 1), 1);
 
         Surface surf = bfc.ToSurface();
         Mesh = surf.ToMesh();
